7aca338e87737a72215791cf76bee26b23b64f35
Kawoon <kawoon13@gmail.com>
2022-10-31 14:20:00 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,240 @@
-import logo from './logo.svg';
-import './App.css';
+import { utils } from 'ethers';
+import React from 'react';
+import { signer, connectWallet, createService, requestService, withdrawRequest, acceptRequest, changeOutcome, claim } from "./chain/chain.js";
+import { findServicesID, getConfirmations, getName, getLocation, getPaymentWei, getWallet, getDistrust, getCompletions, getEmail } from "./chain/chain.js";
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  tab(hidden, shown) {
+    for (let i=0; i < hidden.length; i++) {
+      hidden[i].style.display = "none";
+    }
+    document.getElementsByClassName('serviceBoxes')[0].style.display = "none";
+    for (let i=0; i < shown.length; i++) {
+      shown[i].style.display = "block";
+    }
+  }
+
+  async search(maxSearch, firstID) {
+    let searches = document.getElementById("search").value.split(', ');
+    let foundIDs = [];
+    for (let i=0; i < searches.length; i++) {
+      foundIDs[i] = await findServicesID(searches[i].toString(), maxSearch, firstID);
+    }
+    if (foundIDs.length === 0) {
+      document.getElementsByClassName('serviceBoxes')[0].style.display = "none";
+      return console.log("no services found");
+    }
+
+    let boxAmt = document.getElementsByClassName('serviceBox').length;
+    if (boxAmt > 0) {
+      for (let i=0; i < boxAmt; i++) {
+        document.getElementsByClassName('serviceBoxes')[0].removeChild(document.getElementsByClassName('serviceBox')[0]);
+      }
+    }
+
+    let usedIDs = [];
+    let unused = true;
+    for(let i=0; i < foundIDs.length; i++) {
+      for(let j=0; j < foundIDs[i].length; j++) {
+        for(let k=0; k < usedIDs.length; k++) {
+          if (usedIDs[k] === foundIDs[i][j].toString()) {
+            unused = false;
+            break;
+          }
+        }
+        if (unused) {
+          let fID = foundIDs[i][j].toString();
+          let e = document.createElement('serviceBox');
+          let name = document.createElement('name');
+          let location = document.createElement('location');
+          let payment = document.createElement('payment')
+          let wallet = document.createElement('wallet');
+          let distrust = document.createElement('distrust');
+          let completions = document.createElement('completions');
+          let email = document.createElement('email');
+          let ID = document.createElement('ID');
+          e.setAttribute("class", "serviceBox");
+          name.innerHTML = "name: " + await getName(fID);
+
+          location.innerHTML = "location: " + await getLocation(fID);
+          payment.innerHTML = "payment: " + await getPaymentWei(fID)/10**18 + " ETH";
+          wallet.innerHTML = "wallet: " + await getWallet(fID);
+          wallet.style.fontSize = "4px";
+          distrust.innerHTML = "distrust: " + await getDistrust(fID);
+          completions.innerHTML = "fulfillments: " + await getCompletions(fID);
+          email.innerHTML = "email: " + await getEmail(fID);
+          ID.innerHTML = "ID: " + fID;
+
+          e.appendChild(name);
+          e.appendChild(location);
+          e.appendChild(payment);
+          e.appendChild(wallet);
+          e.appendChild(distrust);
+          e.appendChild(completions);
+          e.appendChild(email);
+          e.appendChild(ID);
+          document.getElementsByClassName('serviceBoxes')[0].appendChild(e);
+          document.getElementsByClassName('serviceBoxes')[0].style.display = "block";
+          usedIDs.push(fID);
+        }
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ul id='ul'>
+          <li><div onClick={(e) => {
+            this.tab(document.getElementsByClassName('bubble'), [document.getElementById('searchBubble'), document.getElementById('confirmationsBubble'), document.getElementsByClassName('serviceBoxes')[0]]);
+            e.preventDefault();
+          }}>search</div></li>
+          <li><div onClick={(e) => {
+            this.tab(document.getElementsByClassName('bubble'), [document.getElementById('requestBubble'), document.getElementById('claimBubble')]);
+            e.preventDefault();
+          }}>client hub</div></li>
+          <li><div onClick={(e) => {
+            this.tab(document.getElementsByClassName('bubble'), [document.getElementById('createServiceBubble'), document.getElementById('acceptRequestBubble'), document.getElementById('claimBubble')]);
+            e.preventDefault();
+          }}>service hub</div></li>
+          <li style={{float: "right"}}><div id='infoToggle' onClick={async() => {
+            let b = document.getElementsByClassName('title');
+            let on = 'rgb(228, 146, 80)';
+            let off = 'var(--color3)';
+            var toggleInfo = b[0].style.color === on;
+            for (let i=0; i < b.length; i++) {
+              if (toggleInfo) {
+                b[i].style.color = off;
+                document.getElementById('infoToggle').style.backgroundColor = '';
+              } else {
+                b[i].style.color = on;
+                document.getElementById('infoToggle').style.backgroundColor = on;
+              }
+            }
+          }}>info</div></li>
+          <li style={{float: "right"}}><div id='connectButton' onClick={async() => {
+            if (connectWallet()) {
+              document.getElementById('connectButton').style.color = getComputedStyle(document.getElementById('ul')).getPropertyValue("--color1");
+            }}}>connect wallet</div></li>
+        </ul>
+
+        <br></br><br></br><br></br><br></br><br></br>
+
+        <div className='all'>
+
+          <div className='bubble' id='searchBubble'>
+            <div className='title' onClick={async() => {
+              let toggleInfo = document.getElementById('infoToggle').style.backgroundColor !== '';
+              if (toggleInfo) {
+                alert('search for a service:\n1: search by name, email, location, wallet, or tags\n2: maximum amount of searches per phrase (phrase1, phrase2, ...)\n3: begin search starting with this ID (use 0 by default)');
+              } else {
+                this.search(document.getElementById('maxSearch').value.toString(), document.getElementById('firstID').value.toString());
+              }}}>search</div>
+            <input className='input' id='search' type="text" placeholder="search: by name, location, or wallet"></input>
+            <input className='input' id='maxSearch' type="number" min="1" placeholder="maximum searches"></input>
+            <input className='input' id='firstID' type="number" min="1" placeholder="first ID: start with this ID"></input>
+          </div>
+
+          <div className='bubble' id='confirmationsBubble'>
+            <div className='title' onClick={async() => {
+              let toggleInfo = document.getElementById('infoToggle').style.backgroundColor !== '';
+              if (toggleInfo) {
+                alert('find out the status of both the client and service:\nhas the other party approved or denied the completion of the service?');
+              } else {
+                getConfirmations(document.getElementById('confirmationsID').value.toString(), document.getElementById('confirmationsWallet').value.toString());
+              }}}>look up status</div>
+            <input className='input' id='confirmationsWallet' type="text" placeholder="client wallet"></input>
+            <input className='input' id='confirmationsID' type="number" min="1" placeholder="service ID"></input>
+          </div>
+
+          <div className='bubble' id='requestBubble'>
+            <div className='title' onClick={async() => {
+              let toggleInfo = document.getElementById('infoToggle').style.backgroundColor !== '';
+              if (toggleInfo) {
+                alert('how to become a client:\n1: use search to find service ID\n2: contact the service provider through email\n3: request service with ID');
+              } else {
+                requestService(document.getElementById('requestID').value.toString());
+              }}}>request service</div>
+              <div className='title' onClick={async() => {
+                let toggleInfo = document.getElementById('infoToggle').style.backgroundColor !== '';
+                if (toggleInfo) {
+                  alert('you may cancel the request for a full refund before the request is accepted. otherwise the proccess will continue');
+                } else {
+                withdrawRequest(document.getElementById('requestID').value.toString());
+              }}}>withdraw request</div>
+            <input className='input' id='requestID' type="text" min="1" placeholder="service ID"></input>
+          </div>
+
+          <div className='bubble' id='createServiceBubble'>
+            <div className="title" onClick={async() => {
+              let toggleInfo = document.getElementById('infoToggle').style.backgroundColor !== '';
+              if (toggleInfo) {
+                alert('the service provider has the power in a dispute. if the service and client disagree on the outcome, the service will still receive the payment but gains a distrust point. the distrust/fulfillment ratio can be found within search');
+              } else {
+                try {
+                  let n = document.getElementsByName('createServiceInput');
+                  let tags = n[1].value.toString().split(', ');
+                  createService(n[0].value, [tags[0], tags[1], tags[2]], n[2].value, signer.getAddress(), utils.parseEther((n[3].value).toString()), n[4].value);
+                } catch {
+                  alert('service must have 3 tags separated by ", "');
+              }}}}>create service</div>
+            <input className="input" name='createServiceInput' placeholder="service name: example"></input>
+            <input className="input" name='createServiceInput' placeholder="3 search tags: ex1, ex2, ex3"></input>
+            <input className="input" name='createServiceInput' placeholder="location: region"></input>
+            <input className="input" name='createServiceInput' min="0" placeholder="payment in ETH: 0.1"></input>
+            <input className="input" name='createServiceInput' placeholder="email: email@email.com"></input>
+          </div>
+
+          <div className='bubble' id='acceptRequestBubble'>
+            <div className='title' onClick={async() => {
+              let toggleInfo = document.getElementById('infoToggle').style.backgroundColor !== '';
+              if (toggleInfo) {
+                alert('to prevent trust exploits, there is a 6% fee on each accepted request. make sure the client is worthy of accepting through email');
+              } else {
+                acceptRequest(document.getElementById('acceptRequestID').value.toString(), document.getElementById('acceptRequestWallet').value.toString());
+              }}}>accept request</div>
+            <input className='input' id='acceptRequestWallet' type="text" placeholder="requested wallet"></input>
+            <input className='input' id='acceptRequestID' type="number" min="1" placeholder="service ID"></input>
+          </div>
+
+          <div className='bubble' id='claimBubble'>
+            <div className='title' onClick={async() => {
+              let toggleInfo = document.getElementById('infoToggle').style.backgroundColor !== '';
+              if (toggleInfo) {
+                alert('after the service is provided, both parties should approve or deny the completion of the service. once a party claims, a 3 day timer begins. when the timer ends, the non-pending party can allocate the payment. ');
+              } else {
+                let b = true;
+                if (document.getElementById('claimComplete').checked) {
+                  b = false;
+                }
+                claim(document.getElementById('claimID').value.toString(), document.getElementById('claimWallet').value.toString(), b);
+              }}}>claim</div>
+            <div className='title' onClick={async() => {
+              let toggleInfo = document.getElementById('infoToggle').style.backgroundColor !== '';
+              if (toggleInfo) {
+                alert('use the claim button first. if a mistake was made while claiming and the outcome has changed, use this button');
+              } else {
+              let b = true;
+              if (document.getElementById('claimComplete').checked) {
+                b = false;
+              }
+              changeOutcome(document.getElementById('claimID').value.toString(), document.getElementById('claimWallet').value.toString(), b);
+              }}}>alter outcome</div>
+            <input className='input' id='claimWallet' type="text" placeholder="client wallet"></input>
+            <input className='input' id='claimID' type="number" min="1" placeholder="service ID"></input>
+            <label htmlFor='claimComplete'>service incomplete<input className='input' id='claimComplete' type='checkbox' style={{padding: '0px'}}></input></label>
+          </div>
+
+          <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+          <div className='serviceBoxes'></div>
+
+        <script src="./chain/chain.js"></script>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/chain/chain.js
@@ -0,0 +1,171 @@
+import { ethers } from 'ethers';
+import { BigNumber } from 'ethers';
+import ABI_SERVICE from './abis/ABI_SERVICE.json';
+import BC_SERVICE from './abis/BC_SERVICE.json';
+
+export const provider = new ethers.providers.Web3Provider(window.ethereum, 'any');
+export var signer;
+const contractAddress = '0xe5c5b261610E5F6C4d290883bccd896fE0bB73D7';
+const errorAlert = 'ERROR:\n1: connect wallet\n2: retry inputs\n3: switch to bsc testnet (chainID: 97)';
+const refreshAlert = 'ERROR: refresh page';
+var cSERVICE;
+
+
+//switch account
+provider.on('accountsChanged', async() => {
+    try {
+        await provider.send("eth_requestAccounts", []);
+        signer = provider.getSigner(0);
+    } catch {
+        alert(refreshAlert);
+    }
+});
+
+export async function connectWallet() {
+    try {
+        if (window.ethereum) {
+            await provider.send("eth_requestAccounts", []);
+            signer = provider.getSigner(0);
+            setContracts(contractAddress);
+            console.log(await signer.getAddress());
+            return true;
+        } else {
+            alert('no wallet found');
+            return false;
+        }
+    } catch {
+        alert(refreshAlert);
+    }
+}
+
+export async function deploy() {
+    cSERVICE = await new ethers.ContractFactory(ABI_SERVICE, BC_SERVICE, signer).deploy();
+}
+
+export async function getContracts() {
+    console.log(cSERVICE);
+}
+
+export async function setContracts(contractAddress) {
+    try {
+        cSERVICE = new ethers.Contract(contractAddress, ABI_SERVICE, signer);
+        getContracts();
+    } catch {
+        alert(refreshAlert);
+    }
+}
+
+export async function createService(name, tags, location, wallet, paymentWei, email) {
+    try {
+        await cSERVICE.createService(name, tags, location, wallet, paymentWei, email);
+    } catch {
+        alert(errorAlert);
+    }
+}
+
+export async function requestService(ID) {
+    try{
+        await cSERVICE.requestService(ID, {value: getPaymentWei(ID)});
+    } catch {
+        alert(errorAlert);
+    }
+}
+
+export async function acceptRequest(ID, clientWallet) {
+    try{
+        let fee = await getPaymentWei(ID) * await cSERVICE.antiExploitFee() / 100;
+        await cSERVICE.acceptRequest(ID, clientWallet, {value: fee});
+    } catch {
+        alert(errorAlert);
+    }
+}
+
+export async function withdrawRequest(ID) {
+    try {
+        await cSERVICE.withdrawRequest(ID);
+    } catch {
+        alert(errorAlert);
+    }
+}
+
+export async function changeOutcome(ID, clientWallet, complete) {
+    try {
+        await cSERVICE.changeOutcome(ID, clientWallet, complete);
+    } catch {
+        alert(errorAlert);
+    }
+}
+
+export async function claim(ID, clientWallet, complete) {
+    try {
+        await cSERVICE.claim(ID, clientWallet, complete);
+    } catch (e) {
+        
+        alert('ERROR:\n3 day pending requirement');
+        getConfirmations(ID, clientWallet);
+    }
+}
+
+export async function findServicesID(search, maxSearch, firstID) {
+    try {
+        let IDs = await cSERVICE.findServicesID(search, maxSearch, firstID);
+        let r = [];
+        for (let i = 0; i < IDs.length; i++) {
+            if (IDs[i].eq(BigNumber.from(0))) {
+                break;
+            }
+            r.push(IDs[i]);
+        }
+        return r;
+    } catch {
+        alert(errorAlert);
+    }
+}
+
+//getters
+export async function getConfirmations(ID, clientWallet) {
+    try {
+        let s = [];
+        let g = await cSERVICE.getConfirmations(ID, clientWallet);
+        for (let i=0; i < g.length; i++) {
+            if (g[i].toString() === '0') {
+                s.push('pending');
+            } else if (g[i].toString() === '1') {
+                s.push('denied');
+            } else if (g[i].toString() === '2') {
+                s.push('approved');
+            }
+        }
+        alert('service: ' + s[0] +'\nclient: ' + s[1]);
+    } catch {
+        alert(errorAlert);
+    }
+}
+
+export async function getName(ID) {
+    return await cSERVICE.getName(ID);
+}
+
+export async function getLocation(ID) {
+    return await cSERVICE.getLocation(ID);
+}
+
+export async function getPaymentWei(ID) {
+    return await cSERVICE.getPaymentWei(ID);
+}
+
+export async function getWallet(ID) {
+    return await cSERVICE.getWallet(ID);
+}
+
+export async function getDistrust(ID) {
+    return await cSERVICE.getDistrust(ID);
+}
+
+export async function getCompletions(ID) {
+    return await cSERVICE.getCompletions(ID);
+}
+
+export async function getEmail(ID) {
+    return await cSERVICE.getEmail(ID);
+}
